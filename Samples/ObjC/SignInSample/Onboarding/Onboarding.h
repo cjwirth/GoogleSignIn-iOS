415784ba88b3ffd0b9726dfda26ea1fcdd698b9f
Caesar Wirth <caesar@duolingo.com>
2024-05-06 13:08:23 -0400

    Add frameworks, which breaks the build

---
+++ Samples/ObjC/SignInSample/Onboarding/Onboarding.h
@@ -0,0 +1,19 @@
+//
+//  Onboarding.h
+//  Onboarding
+//
+//  Created by Caesar Wirth on 2024-05-06.
+//  Copyright © 2024 Google Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Onboarding.
+FOUNDATION_EXPORT double OnboardingVersionNumber;
+
+//! Project version string for Onboarding.
+FOUNDATION_EXPORT const unsigned char OnboardingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Onboarding/PublicHeader.h>
+
+
